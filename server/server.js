a603084ec7d71979e388ab2b01d4c62721f1599a
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-12-14 01:36:33 -0700

    testing deployment demo'

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`public`))
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
