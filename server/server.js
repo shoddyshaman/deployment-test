062176487246fbcd3df74a41e04d2b3bc020eabf
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-12-14 00:06:28 -0700

    testing deployment demo'

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
+app.get('/',(req,res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
 app.listen(4000,
    () => console.log(`server running on 4000`)
 )
